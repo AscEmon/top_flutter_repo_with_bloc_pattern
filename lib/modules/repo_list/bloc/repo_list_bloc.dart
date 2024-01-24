1186669d8a7ac3362ae5417cbf201994c50d7d27
Abu Sayed <sayedemon53@gmail.com>
2024-01-24 22:47:12 +0600

    feat: :construction: fetch repo list with param using bloc pattern

--- lib/modules/repo_list/bloc/repo_list_bloc.dart
+++ lib/modules/repo_list/bloc/repo_list_bloc.dart
@@ -1,8 +1,36 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:top_flutter_repo/modules/repo_list/bloc/repo_list_state.dart';
+import '/modules/repo_list/bloc/repo_list_state.dart';
 
+import '../../../utils/enum.dart';
+import '../model/repo_list_response.dart';
+import '../repository/repo_list_interface.dart';
+import '../repository/repo_list_repository.dart';
 import 'repo_list_event.dart';
 
 class RepoListBloc extends Bloc<RepoListEvent, RepoListState> {
-  RepoListBloc() : super(const RepoListState()) {}
+  final IRepoListRepository _iRepoListRepository = RepoListRepository();
+  RepoListBloc() : super(const RepoListState()) {
+    on<LoadRepoListEvent>(_fetchRepos);
+  }
+  void _fetchRepos(LoadRepoListEvent event, Emitter<RepoListState> emit) async {
+    try {
+      List<RepositoryItem> result =
+          await _iRepoListRepository.fetchRepoList(_setParams());
+      emit(
+        state.copyWith(
+          fetchRepoStatus: AppStatus.success,
+          repoListItem: result,
+        ),
+      );
+    } catch (error) {
+      emit(state.copyWith(fetchRepoStatus: AppStatus.error));
+    }
+  }
+
+  Map<String, dynamic> _setParams() {
+    final map = <String, dynamic>{};
+    map["q"] = "Flutter";
+    map["per_page"] = 10;
+    return map;
+  }
 }
--- lib/modules/repo_list/view/repo_list_screen.dart
+++ lib/modules/repo_list/view/repo_list_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:top_flutter_repo/global/widget/global_appbar.dart';
 import 'package:top_flutter_repo/global/widget/global_text.dart';
+import 'package:top_flutter_repo/modules/repo_list/bloc/repo_list_bloc.dart';
+import 'package:top_flutter_repo/modules/repo_list/bloc/repo_list_event.dart';
 
 class RepoListScreen extends StatefulWidget {
   const RepoListScreen({super.key});
@@ -10,6 +13,12 @@ class RepoListScreen extends StatefulWidget {
 }
 
 class _RepoListScreenState extends State<RepoListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<RepoListBloc>().add(LoadRepoListEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
